ad252e6ac92681a93a505b6f20558a78283d1976
Yixuan Chen <yc4153@columbia.edu>
2023-05-16 21:53:29 -0400

    Create analytics_query.sql

---
+++ UberDataEngineering/analytics_query.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE TABLE `braided-topic-386622.uber_data_engineering_silvia.tbl_analytics` AS (
+SELECT 
+f.VendorID,
+d.tpep_pickup_datetime,
+d.tpep_dropoff_datetime,
+p.passenger_count,
+t.trip_distance,
+r.rate_code_name,
+pick.pickup_latitude,
+pick.pickup_longitude,
+drop.dropoff_latitude,
+drop.dropoff_longitude,
+pay.payment_type_name,
+f.fare_amount,
+f.extra,
+f.mta_tax,
+f.tip_amount,
+f.tolls_amount,
+f.improvement_surcharge,
+f.total_amount
+FROM 
+
+`braided-topic-386622.uber_data_engineering_silvia.fact_table` f
+JOIN `braided-topic-386622.uber_data_engineering_silvia.datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `braided-topic-386622.uber_data_engineering_silvia.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `braided-topic-386622.uber_data_engineering_silvia.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `braided-topic-386622.uber_data_engineering_silvia.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN `braided-topic-386622.uber_data_engineering_silvia.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `braided-topic-386622.uber_data_engineering_silvia.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `braided-topic-386622.uber_data_engineering_silvia.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+;
